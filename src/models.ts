aec380a9d26a96600a5a8790e5cdd8a8fe5dae3c
rakr0819 <raman.krauchanka@netcracker.com>
2024-10-19 23:29:14 +0300

    feat: impl dev/prod modes

--- src/models.ts
+++ src/models.ts
@@ -1,8 +1,9 @@
 export interface IUser extends IUserInput {
   id: string;
+}
+
+export type IUserInput = {
   username: string;
   age: number;
   hobbies: string[];
 }
-
-export type IUserInput = Omit<IUser, 'id'>;
